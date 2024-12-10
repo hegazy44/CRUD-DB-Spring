629098bb2c1b956af9afc9f2892af20df349d880
hegazy mohamed <hegazymohamed2017@gmail.com>
2024-12-10 12:17:24 +0200

    Last Update Version

--- src/main/java/com/example/Crud/controller/EmployeeController.java
+++ src/main/java/com/example/Crud/controller/EmployeeController.java
@@ -3,8 +3,7 @@ import com.example.Crud.entity.Employee;
 import com.example.Crud.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +29,36 @@ public class EmployeeController {
         theModel.addAttribute("employees", theEmployees);
 
         return "list-employees";
+    }
+
+        // add new employee
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        // call object
+        Employee employee = new Employee();
+        //using model to retrieve data
+        model.addAttribute("employee",employee);
+        return "employees/employee-form";
+    }
+
+    // Save Employee
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.save(employee);
+      return "redirect:/employees/list";
+    }
+
+    @GetMapping("/ShowFormForUpdate")
+    public String updateEmployee(@RequestParam("employeeId") int theID , Model model){
+        Employee employee = employeeService.findById(theID);
+        model.addAttribute(employee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") int theId){
+        employeeService.deleteById(theId);
+        return "redirect:/employees/list";
     }
 }
 
--- src/main/java/com/example/Crud/dao/EmployeeRepository.java
+++ src/main/java/com/example/Crud/dao/EmployeeRepository.java
@@ -3,8 +3,13 @@ package com.example.Crud.dao;
 import com.example.Crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    //method to sort ASC By FirstName
+    public List<Employee> findAllByOrderByFirstNameAsc();
 
 }
