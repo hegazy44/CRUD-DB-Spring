c9a265f8545e004dacc5f0e04e9aadeb79bfc5eb
hegazy mohamed <hegazymohamed2017@gmail.com>
2024-12-10 09:39:07 +0200

    GET ALL DATA

---
+++ src/main/java/com/example/Crud/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.Crud.service;
+
+
+import com.example.Crud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
